36245bc2e5a0368ba8d1fae1ac9c6d28c9376738
Oleg <kovalyovop@yandex.ru>
2024-08-15 21:21:25 +0300

    15.08.2024

---
+++ src/test/java/com/LearningToJava/LearningToJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LearningToJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningToJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

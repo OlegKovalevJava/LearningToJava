c6d2bb2b90f9c944f3047e93e599858ee44fcc0a
Oleg <kovalyovop@yandex.ru>
2024-08-20 16:58:52 +0300

    21.08.2024

--- src/main/java/com/LearningToJava/Main.java
+++ src/main/java/com/LearningToJava/Main.java
@@ -3,5 +3,11 @@ package com.LearningToJava;
 public class Main {
     public static void main(String[] args) {
 
+        goPrivetMir();
+
+    }
+
+    public static void goPrivetMir() {
+        System.out.println("privet Mir !!!!!");
     }
 }

857167403aa3272b85952addd06fc271b579b5ed
Oleg <kovalyovop@yandex.ru>
2024-08-21 22:04:33 +0300

    21.08.2024

---
+++ src/main/java/com/LearningToJava/Cookie.java
@@ -0,0 +1,23 @@
+package com.LearningToJava;
+
+public class Cookie {
+
+    public void goAndSaveThePrincess() {
+        sharpenBlade();
+        getFood();
+        assembleTeam();
+        System.out.println("Да иду уже...");
+    }
+
+    private void sharpenBlade() {
+        System.out.println("Точим меч");
+    }
+    private void getFood() {
+        System.out.println("Собираем консервы");
+    }
+    private void assembleTeam() {
+        System.out.println("Будим оруженосца");
+    }
+
+
+}

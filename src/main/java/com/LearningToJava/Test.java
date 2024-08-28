c263f3c571de40b73a548fe3399b1363782b6826
Oleg <kovalyovop@yandex.ru>
2024-08-29 00:32:16 +0300

    29.08.2024

--- src/main/java/com/LearningToJava/Test.java
+++ src/main/java/com/LearningToJava/Test.java
@@ -4,34 +4,79 @@ public class Test {
 
     public static void main(String[] args) {
 
-        int[] numbers = {6, 1, 5, 2, 4, 3};
-        int[] numbers1 = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-        int[] numbers2 = {1, 2, 0, 0, 0, 0, 0, 3};
-        int[] numbers3 = {0, 0, 0};
-        int[] numbers4 = {8, 9, 7, 8, 6, 7, 5, 6};
+        int[] numbers1 = {10, 1, 2, 1, 2, 10};
+        int[] numbers2 = {0, 0, 0, 0};
+        int[] numbers3 = {1, 9, 8, 9, 8, 9, 2, 3};
+        int[] numbers4 = {1, 9, 8, 9, 8, 9, 8, 9};
+        int[] numbers5 = {8, 9, 8, 9, 8, 9, 8, 9};
+        int[] numbers6 = {8, 9};
+        int[] numbers7 = {8, 9, 7, 8, 6, 7, 5, 6};
 
-        System.out.println(goTest(numbers));
         System.out.println(goTest(numbers1));
         System.out.println(goTest(numbers2));
         System.out.println(goTest(numbers3));
         System.out.println(goTest(numbers4));
+        System.out.println(goTest(numbers5));
+        System.out.println(goTest(numbers6));
+        System.out.println(goTest(numbers7));
+
+        System.out.println(checkInc(numbers1));
+        System.out.println(checkInc(numbers2));
+        System.out.println(checkInc(numbers3));
+        System.out.println(checkInc(numbers4));
+        System.out.println(checkInc(numbers5));
+        System.out.println(checkInc(numbers6));
+        System.out.println(checkInc(numbers7));
     }
 
     public static boolean goTest(int[] numbers) {
-        int count = 0;
 
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = i + 1; j < numbers.length; j++) {
+        if (numbers.length < 3) {
+            return false;
+        }
 
-                int numberFirst = numbers[i];
-                int numberSecond = numbers[j];
+        int firstCounter = 0;
+        int secondCounter = 0;
 
-                if (numberFirst <= numberSecond) {
+        for (int i = 0; i < numbers.length - 1; i++) {
+            firstCounter++;
 
+            if (numbers[i] <= numbers[i + 1]) {
+
+                for (int j = firstCounter; j < numbers.length - 1; j++) {
+                    if (numbers[firstCounter] <= numbers[j + 1]) {
+                        secondCounter++;
+                        break;
+                    }
                 }
             }
         }
 
-        return count >= 2;
+        return secondCounter > 0;
     }
+
+    public static boolean checkInc(int[] nums) {
+        int firstMin = Integer.MAX_VALUE;
+        int secondMin = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num < firstMin) {
+                firstMin = num;
+            } else if (num < secondMin) {
+                secondMin = num;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 }
+
+
+
+
+
+
+

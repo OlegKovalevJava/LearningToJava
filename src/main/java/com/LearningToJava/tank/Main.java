de350e0d8cffc0de38757a95e39d2a3449dca291
Oleg <kovalyovop@yandex.ru>
2024-09-18 00:32:04 +0300

    18.09.2024

--- src/main/java/com/LearningToJava/tank/Main.java
+++
@@ -1,9 +0,0 @@
-package com.LearningToJava.tank;
-
-public class Main {
-    public static void main(String[] args) {
-
-        SuperAmphibianTank superAmphibianTank = new SuperAmphibianTank();
-        superAmphibianTank.test();
-    }
-}

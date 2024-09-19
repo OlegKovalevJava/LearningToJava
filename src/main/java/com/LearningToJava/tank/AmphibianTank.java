a957153d2217a2f9501729a6b5b737e9a6709cd3
Oleg <kovalyovop@yandex.ru>
2024-09-19 18:47:30 +0300

    19.09.2024

--- src/main/java/com/LearningToJava/tank/AmphibianTank.java
+++ src/main/java/com/LearningToJava/tank/AmphibianTank.java
@@ -1,10 +1,10 @@
 package com.LearningToJava.tank;
 
+import lombok.Getter;
+
+@Getter
 public class AmphibianTank extends Tank {
 
     int a = 1;
 
-    public int getA() {
-        return a;
-    }
 }

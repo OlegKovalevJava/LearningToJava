31b1c2152e4d049ce1c2ba301802e45568a98aed
Oleg <kovalyovop@yandex.ru>
2024-10-18 21:16:36 +0300

    18.10.2024

--- src/main/java/com/LearningToJava/test/Main.java
+++ src/main/java/com/LearningToJava/test/Main.java
@@ -1,13 +1,17 @@
 package com.LearningToJava.test;
 
 import java.io.File;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        File file = new File("C:\\Test", "Test.txt");
-        System.out.println(file.exists());//true
+        File file = new File("C:\\Test");
+        String[] files = file.list();
+
+        System.out.println(Arrays.toString(files));
+
     }
 
 }

8b98bc4e945d1213f133fb4f155c00075932df46
Oleg <kovalyovop@yandex.ru>
2024-10-25 10:52:00 +0300

    25.10.2024

--- src/main/java/com/LearningToJava/Main.java
+++ src/main/java/com/LearningToJava/Main.java
@@ -1,13 +1,12 @@
 package com.LearningToJava;
 
-import java.io.File;
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        File file = new File("C:\\Test");
-        String[] files = file.list();
-        System.out.println(files.length);
+    public static void main(String[] args) {
+
+        Person unknown = new Person(50);
+        System.out.println(unknown);
+
+
     }
 
 }

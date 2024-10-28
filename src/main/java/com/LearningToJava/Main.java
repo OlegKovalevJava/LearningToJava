f81eb7ba3f54d1e083f167541e5f4b5e5c68522f
Oleg <kovalyovop@yandex.ru>
2024-10-28 12:30:44 +0300

    28.10.2024

--- src/main/java/com/LearningToJava/Main.java
+++ src/main/java/com/LearningToJava/Main.java
@@ -3,10 +3,14 @@ package com.LearningToJava;
 public class Main {
     public static void main(String[] args) {
 
-        Person unknown = new Person(50);
-        System.out.println(unknown);
+        int[] array = {1, 2, -1, 5, 3};
 
+        int s = 0;
+        for (int i = 0; i <= array.length - 1; i++) {
+            s += array[i];
+        }
 
+        System.out.println(s);
     }
 
 }

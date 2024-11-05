6ab875c0112a5e276a6fc3ac72c1db752b38a2fe
Oleg <kovalyovop@yandex.ru>
2024-11-05 20:56:03 +0300

    05.11.2024

---
+++ src/main/java/com/LearningToJava/Day.java
@@ -0,0 +1,11 @@
+package com.LearningToJava;
+
+public enum Day {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
--- src/main/java/com/LearningToJava/Main.java
+++ src/main/java/com/LearningToJava/Main.java
@@ -1,27 +1,10 @@
 package com.LearningToJava;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
-
 public class Main {
     public static void main(String[] args) {
-        int numberOfTasks = 5;
 
-        Queue toDoQueue = new ArrayDeque(numberOfTasks);
+        Day current = Day.MONDAY;
+        System.out.println(current);    // MONDAY
 
-        Boss boss = new Boss(toDoQueue);
-
-        Worker worker = new Worker(toDoQueue);
-
-        for (int i = 1; i <= numberOfTasks; i++) {
-            boss.giveTask("Задание " + i);
-        }
-
-        System.out.println("Начальник закончил давать задания");
-
-
-        for (int i = 1; i <= numberOfTasks + 1; i++) {
-            worker.takeTask();
-        }
     }
 }

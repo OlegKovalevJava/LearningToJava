e9546a72811587d6a836d040d3f8bc26e4e1621d
Oleg <kovalyovop@yandex.ru>
2024-11-06 14:33:36 +0300

    06.11.2024

--- src/main/java/com/LearningToJava/Main.java
+++ src/main/java/com/LearningToJava/Main.java
@@ -3,8 +3,5 @@ package com.LearningToJava;
 public class Main {
     public static void main(String[] args) {
 
-        Day current = Day.MONDAY;
-        System.out.println(current);    // MONDAY
-
     }
 }

b08549c6ef706e4fbe60a249bee1d04aa85e1812
Oleg <kovalyovop@yandex.ru>
2024-11-08 20:33:57 +0300

    08.11.2024

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Number number = new Number() {
+            @Override
+            public int intValue() {
+                return 0;
+            }
+
+            @Override
+            public long longValue() {
+                return 0;
+            }
+
+            @Override
+            public float floatValue() {
+                return 0;
+            }
+
+            @Override
+            public double doubleValue() {
+                return 0;
+            }
+        }
+
+    }
+}

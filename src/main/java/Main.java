cd7543b08318852c3b1bf65f6c894727aa573cf6
Oleg <kovalyovop@yandex.ru>
2024-11-11 12:46:43 +0300

    11.11.2024

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>();
 
-        Number number = new Number() {
-            @Override
-            public int intValue() {
-                return 0;
-            }
-
-            @Override
-            public long longValue() {
-                return 0;
-            }
-
-            @Override
-            public float floatValue() {
-                return 0;
-            }
-
-            @Override
-            public double doubleValue() {
-                return 0;
-            }
-        }
-
+        System.out.println(stringList.isEmpty());
+        stringList.add("strings");
+        System.out.println(stringList.isEmpty());
     }
+
+    public static List<Integer> go(List<Integer> integerList) {
+        return integerList;
+    }
+
+
 }

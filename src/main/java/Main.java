d5372fa97a7efdeb61bcda518b8aea957deb2cc7
Oleg <kovalyovop@yandex.ru>
2024-11-12 12:41:04 +0300

    12.11.2024

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,5 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        List<String> stringList = new ArrayList<>();
 
-        System.out.println(stringList.isEmpty());
-        stringList.add("strings");
-        System.out.println(stringList.isEmpty());
     }
-
-    public static List<Integer> go(List<Integer> integerList) {
-        return integerList;
-    }
-
-
 }

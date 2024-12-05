987e7eb68316a316a41e744189de023c8639bead
Oleg <kovalyovop@yandex.ru>
2024-12-05 13:41:04 +0300

    05.12.2024

---
+++ src/main/java/Immutable.java
@@ -0,0 +1,9 @@
+public final class Immutable {
+
+    private final int age;
+
+    public Immutable(int age) {
+        this.age = age;
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = new int[3];
 
-        try {
-            numbers[6] = 45;
-            numbers[6] = Integer.parseInt("gfd");
-        } catch (ArrayIndexOutOfBoundsException ex) {
-            System.out.println("Выход за пределы массива");
-        } catch (NumberFormatException ex) {
-            System.out.println("Ошибка преобразования из строки в число");
-        }
+        byte a = 100;
+        Byte b = a;
+
     }
 }

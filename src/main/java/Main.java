47e4b8084a01f797809e4a6f842f4e1f0f8967db
Oleg <kovalyovop@yandex.ru>
2024-12-10 16:30:00 +0300

    10.12.2024

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,5 @@
 public class Main {
     public static void main(String[] args) {
 
-        Integer i = 10;
-
-        byte b = i.byteValue();
-
-        Boolean n = null;
-
-        Integer c = null;
-
     }
 }

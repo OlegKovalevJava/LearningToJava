64e5d2a020eefcac698c9bf354a5094e828d1ce5
Oleg <kovalyovop@yandex.ru>
2024-12-19 18:18:24 +0300

    19.12.2024

--- src/main/java/com/LearningToJava/Quest.java
+++
@@ -1,8 +0,0 @@
-package com.LearningToJava;
-
-public class Quest {
-    public static void main(String[] args) {
-        String s = "String 12121212";
-        System.out.println(s instanceof String);
-    }
-}

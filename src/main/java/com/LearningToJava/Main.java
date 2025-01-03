f0609cf823b6088b3ed94b6a3dc387dfce157426
Oleg <kovalyovop@yandex.ru>
2025-01-03 14:55:10 +0300

    03.01.2025

--- src/main/java/com/LearningToJava/Main.java
+++ src/main/java/com/LearningToJava/Main.java
@@ -1,7 +1,13 @@
 package com.LearningToJava;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        List<Test> testList = new ArrayList<>();
+        Test test = new Test(30, "Fox", new BigDecimal(10000));
+        testList.add(test);
     }
 }
--- src/main/java/com/LearningToJava/Test.java
+++ src/main/java/com/LearningToJava/Test.java
@@ -1,4 +1,15 @@
 package com.LearningToJava;
 
+import java.math.BigDecimal;
+
 public class Test {
+    private int age;
+    private String FIO;
+    private BigDecimal balance;
+
+    public Test(int age, String FIO, BigDecimal balance) {
+        this.age = age;
+        this.FIO = FIO;
+        this.balance = balance;
+    }
 }

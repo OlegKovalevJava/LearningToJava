2751e07e0196603ad614473645f754ce91222b6c
Oleg <kovalyovop@yandex.ru>
2025-01-04 16:25:42 +0300

    04.01.2025

--- src/main/java/com/LearningToJava/Main.java
+++ src/main/java/com/LearningToJava/Main.java
@@ -7,7 +7,14 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Test> testList = new ArrayList<>();
-        Test test = new Test(30, "Fox", new BigDecimal(10000));
-        testList.add(test);
+
+        testList.add(0, new Test(20, "Fox", new BigDecimal(555)));
+        testList.add(1, new Test(30, "Mox", new BigDecimal(111111)));
+        testList.add(2, new Test(20, "Fox", new BigDecimal(99999999.99)));
+
+        for (Test tests: testList) {
+            System.out.println(testList);
+        }
+
     }
 }
--- src/main/java/com/LearningToJava/Test.java
+++ src/main/java/com/LearningToJava/Test.java
@@ -7,9 +7,18 @@ public class Test {
     private String FIO;
     private BigDecimal balance;
 
+    public Test(String FIO) {
+        this.FIO = FIO;
+    }
+
     public Test(int age, String FIO, BigDecimal balance) {
         this.age = age;
         this.FIO = FIO;
         this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Test : age=" + age + ", FIO = " + FIO + ", balance=" + balance;
     }
 }

2bb1d5eb85a9ed8b4b4a4f2d10fbddd85b6d8a79
Oleg <kovalyovop@yandex.ru>
2025-01-07 22:03:25 +0300

    07.01.2025

--- src/main/java/com/LearningToJava/Test.java
+++ src/main/java/com/LearningToJava/Test.java
@@ -19,6 +19,6 @@ public class Test {
 
     @Override
     public String toString() {
-        return "Test : age=" + age + ", FIO = " + FIO + ", balance=" + balance;
+        return "Test : age =" + age + ", FIO = " + FIO + ", balance =" + balance;
     }
 }

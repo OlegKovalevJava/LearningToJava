9d70ad6a29e39d812b9251ecb8048337efb386d7
Oleg <kovalyovop@yandex.ru>
2025-01-10 00:44:30 +0300

    10.01.2025

---
+++ src/main/java/com/LearningToJava/Gerbil.java
@@ -0,0 +1,13 @@
+package com.LearningToJava;
+
+public class Gerbil {
+    int gerbilNumber;
+
+    public Gerbil(int gerbilNumber) {
+        this.gerbilNumber = gerbilNumber;
+    }
+
+    public void hop() {
+        System.out.println(gerbilNumber + " Вот это номер!");
+    }
+}
--- src/main/java/com/LearningToJava/Main.java
+++ src/main/java/com/LearningToJava/Main.java
@@ -1,8 +1,23 @@
 package com.LearningToJava;
 
+import java.util.ArrayList;
+
 public class Main {
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
 
+        ArrayList<Gerbil> gerbils = new ArrayList<>();
 
+        for (int i = 0; i < 4; i++) {
+            gerbils.add(new Gerbil(i));
+        }
+
+        for (Gerbil gerbil : gerbils) {
+            System.out.println(gerbil.gerbilNumber);
+        }
+
+        for (Gerbil c : gerbils) {
+            c.hop();
+        }
     }
 }

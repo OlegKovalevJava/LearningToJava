c2d2eaed6238abae8b345b2d77da9430b2652b25
Oleg <kovalyovop@yandex.ru>
2025-01-13 19:00:54 +0300

    13.01.2025

--- src/main/java/com/LearningToJava/Main.java
+++ src/main/java/com/LearningToJava/Main.java
@@ -1,26 +1,13 @@
 package com.LearningToJava;
 
-import java.util.ArrayList;
+import java.util.TreeSet;
 
 public class Main {
-    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
 
-        ArrayList<Gerbil> gerbils = new ArrayList<>();
+        TreeSet<String> stringTreeSet = new TreeSet<>();
+        stringTreeSet.add("Лист");
 
-        for (int i = 0; i < 4; i++) {
-            gerbils.add(new Gerbil(i));
-        }
 
-        for (Gerbil gerbil : gerbils) {
-            System.out.println(gerbil.gerbilNumber);
-        }
-
-        for (Gerbil c : gerbils) {
-            c.hop();
-        }
-
-        Apple apple = new Gala();
-        apple.say();
     }
 }

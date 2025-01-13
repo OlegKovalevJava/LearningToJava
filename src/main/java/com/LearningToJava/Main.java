60dd65e8e295039d37cf314200ab9f0f83eb41a4
Oleg <kovalyovop@yandex.ru>
2025-01-14 00:31:30 +0300

    14.01.2025

--- src/main/java/com/LearningToJava/Main.java
+++ src/main/java/com/LearningToJava/Main.java
@@ -1,13 +1,7 @@
 package com.LearningToJava;
 
-import java.util.TreeSet;
-
 public class Main {
     public static void main(String[] args) {
-
-        TreeSet<String> stringTreeSet = new TreeSet<>();
-        stringTreeSet.add("Лист");
-
 
     }
 }

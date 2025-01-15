acafff91c1123476092b587e043a690fd85f3916
Oleg <kovalyovop@yandex.ru>
2025-01-15 18:56:25 +0300

    15.01.2025

--- src/main/java/com/LearningToJava/Main.java
+++ src/main/java/com/LearningToJava/Main.java
@@ -1,7 +1,20 @@
 package com.LearningToJava;
 
+import java.util.Collection;
+
 public class Main {
     public static void main(String[] args) {
 
     }
+
+    static Collection fill(Collection<String> collection) {
+        collection.add("rot");
+        collection.add("cat");
+        collection.add("map");
+        collection.add("hack");
+
+        return collection;
+    }
+
+
 }

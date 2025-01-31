bc5e6b435b6e7f7a56198e21c1dada1e19dcab8d
Oleg <kovalyovop@yandex.ru>
2025-01-31 15:39:16 +0300

    31.01.2025

--- src/main/java/com/LearningToJava/Main.java
+++ src/main/java/com/LearningToJava/Main.java
@@ -3,7 +3,5 @@ package com.LearningToJava;
 public class Main {
     public static void main(String[] args) {
 
-        var tvOne = new TV();
-
     }
 }
--- src/main/java/com/LearningToJava/TV.java
+++
@@ -1,4 +0,0 @@
-package com.LearningToJava;
-
-public record TV() {
-}

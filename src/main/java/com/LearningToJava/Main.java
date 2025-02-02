a4cee47ff495c4f3e1eba66609912ef091c53631
Oleg <kovalyovop@yandex.ru>
2025-02-02 17:20:30 +0300

    02.02.2025

--- src/main/java/com/LearningToJava/Main.java
+++ src/main/java/com/LearningToJava/Main.java
@@ -1,9 +1,13 @@
 package com.LearningToJava;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
         var str = "word";
+
+        var stringsList = new ArrayList<>();
 
     }
 }

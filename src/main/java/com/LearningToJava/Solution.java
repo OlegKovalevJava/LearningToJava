1fd6334dee9e7d6e8240b4e46b5b9cd225350f22
Oleg <kovalyovop@yandex.ru>
2025-02-07 13:57:12 +0300

    07.02.2025

---
+++ src/main/java/com/LearningToJava/Solution.java
@@ -0,0 +1,35 @@
+package com.LearningToJava;
+
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) { // через циклы
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return new int[0];
+    }
+
+    //nums = [2,7,11,15], target = 9
+
+    public int[] twoSum2(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (map.containsKey(target - nums[i]) && i != map.get(target - nums[i])) {
+                return new int[]{i, map.get(target - nums[i])};
+            }
+
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/LearningToJava/Solution1.java
@@ -0,0 +1,20 @@
+package com.LearningToJava;
+
+class Solution1 {
+
+    public int maxAscendingSum(int[] nums) {
+        int result = 0;
+        int t = 0;
+
+        for (int i = 0; i < nums.length; ++i) {
+            if (i == 0 || nums[i] > nums[i - 1]) {
+                t += nums[i];
+                result = Math.max(result, t);
+            } else {
+                t = nums[i];
+            }
+        }
+
+        return result;
+    }
+}

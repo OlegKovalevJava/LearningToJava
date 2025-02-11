f3476b84369483ba79ff76e9fffadf8d9358ac5f
Oleg <kovalyovop@yandex.ru>
2025-02-11 19:42:05 +0300

    11.02.2025

--- src/main/java/com/LearningToJava/Palindrome.java
+++ src/main/java/com/LearningToJava/Palindrome.java
@@ -2,13 +2,14 @@ package com.LearningToJava;
 
 public class Palindrome {
     public boolean isPalindrome(int x) {
+
         int originalNumber = x;
         int reversedNumber = 0;
 
         while (x > 0) {
             int digit = x % 10;
-            reversedNumber = reversedNumber * 10 + digit;
-            x /= 10;
+            reversedNumber = (reversedNumber * 10) + digit;
+            x = x / 10;
         }
 
         return originalNumber == reversedNumber;

d9555a629730843eaf6ceee190a99336dc7aceaa
Oleg <kovalyovop@yandex.ru>
2025-02-14 17:25:51 +0300

    14.02.2025

--- src/main/java/com/LearningToJava/Main.java
+++ src/main/java/com/LearningToJava/Main.java
@@ -1,28 +1,7 @@
 package com.LearningToJava;
 
-import java.sql.SQLData;
-import java.sql.SQLException;
-import java.sql.SQLInput;
-import java.sql.SQLOutput;
-
 public class Main {
     public static void main(String[] args) {
 
-        SQLData sqlData = new SQLData() {
-            @Override
-            public String getSQLTypeName() throws SQLException {
-                return "";
-            }
-
-            @Override
-            public void readSQL(SQLInput stream, String typeName) throws SQLException {
-
-            }
-
-            @Override
-            public void writeSQL(SQLOutput stream) throws SQLException {
-
-            }
-        }
     }
 }

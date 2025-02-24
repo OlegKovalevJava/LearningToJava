ca165e029fdb4ae3164b6967aec476396ed99397
Oleg <kovalyovop@yandex.ru>
2025-02-24 09:38:04 +0300

    24.02.2025

--- src/main/java/com/LearningToJava/Main.java
+++ src/main/java/com/LearningToJava/Main.java
@@ -1,13 +1,13 @@
 package com.LearningToJava;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
 
-        TwoSum twoSum = new TwoSum();
-        int[] nums = {2, 3, 3, 2};
-        System.out.println(Arrays.toString(twoSum.twoSum(nums, 10)));
+        String string = "HelloString";
+        StringBuffer stringBuffer = new StringBuffer("HelloStringBuffer");
+        StringBuilder stringBuilder = new StringBuilder("HelloStringBuilder");
+
+
 
     }
 }
